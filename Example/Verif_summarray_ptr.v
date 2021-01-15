f2d25a8759eedd3c6d0e494a81938f1b496ab007
Qinshi Wang <qinshiw@cs.princeton.edu>
2021-01-16 00:02:53 +0800

    pointer comparison with exposed block number

--- Example/Verif_summarray_ptr.v
+++ Example/Verif_summarray_ptr.v
@@ -1,6 +1,8 @@
 Require Import VST.floyd.proofauto.
 Require Import sumarray_ptr.
 
+Open Scope Z_scope.
+
 Set Bullet Behavior "Strict Subproofs".
 
 Instance CompSpecs : compspecs.
@@ -45,14 +47,41 @@ Lemma body_sumarray: semax_body Vprog Gprog f_sumarray sumarray_spec.
 Proof.
   start_function. forward. forward. forward.
   (*Is this the right way to refer to curr?*)
+  assert (0 < size) by admit.
   forward_while
-  (EX i: Z,
-   PROP (0 <= i <= size)
-   LOCAL (temp _end (force_val (sem_binary_operation' Oadd (tptr tuint) tint a (Vint (Int.repr size))));
-      temp _curr (force_val (sem_binary_operation' Oadd (tptr tuint) tint a (Vint (Int.repr i))));
+  (EX (i: Z) b ,
+   PROP (0 <= i <= size; a = (Vptr b (Ptrofs.repr 0)))
+   LOCAL (temp _end (Vptr b (Ptrofs.repr (size * 4)));
+      temp _curr (Vptr b (Ptrofs.repr (i * 4)));
       temp _a a; temp _n (Vint (Int.repr size));
       temp _s (Vint (Int.repr (sum_Z (sublist 0 i contents)))))
    SEP (data_at sh (tarray tuint size) (map Vint (map Int.repr contents)) a)).
-  - Exists 0%Z. entailer!.
-  - (*entailer!. simpl. Search denote_tc_test_order. rewrite isptr_denote_tc_test_order. *) 
+  - admit.
+    (* Exists 0%Z. entailer!. *)
+  - 
+    (* [0, n) 0 .. n-1
+    valid: Vptr b 0 .. Vptr b (n-1)
+    weak_valid: Vptr b 0 .. Vptr b (n-1), Vptr b n *)
+    entailer!. unfold test_order_ptrs. simpl.
+    destruct (proj_sumbool (peq b b)) eqn:Hpeq; unfold proj_sumbool in Hpeq; rewrite peq_true in Hpeq;
+      try discriminate; clear Hpeq.
+    sep_apply data_at_memory_block.
+    apply andp_right.
+    + sep_eapply memory_block_weak_valid_pointer.
+      * instantiate (1 := (i * 4)).
+        simpl. Zlength_solve.
+      * simpl. Zlength_solve.
+      * apply readable_nonidentity; auto.
+      * apply derives_refl'. f_equal.
+        simpl. f_equal.
+        admit.
+    + sep_eapply memory_block_weak_valid_pointer.
+    * instantiate (1 := (Zlength contents) * 4).
+      simpl. list_solve.
+    * simpl. Zlength_solve.
+    * apply readable_nonidentity; auto.
+    * apply derives_refl'. f_equal.
+      simpl. f_equal.
+      admit.
+  (*entailer!. simpl. Search denote_tc_test_order. rewrite isptr_denote_tc_test_order. *) 
 Admitted.
