3e48c61f3a9cc1a56b87a43086543abe45308597
Qinshi Wang <qinshiw@cs.princeton.edu>
2021-01-16 00:33:07 +0800

    pointer comparison with offset_val

--- Example/Verif_summarray_ptr.v
+++ Example/Verif_summarray_ptr.v
@@ -43,45 +43,87 @@ Proof.
   intros p1 p2 Hptr1 Hptr2. destruct p1; destruct Hptr1. destruct p2; destruct Hptr2. reflexivity.
 Qed.
 
+Lemma isptr_offset_val_sameblock : forall p i,
+  isptr p ->
+  sameblock p (offset_val i p) = true.
+Proof.
+  intros. destruct p; destruct H.
+  simpl. unfold proj_sumbool. apply peq_true.
+Qed.
+
+Lemma sameblock_refl : forall p,
+  isptr p ->
+  sameblock p p = true.
+Proof.
+  intros.
+  destruct p; destruct H.
+  simpl.
+  apply peq_true.
+Qed.
+
+Lemma sameblock_symm : forall p1 p2,
+  sameblock p1 p2 = true ->
+  sameblock p2 p1 = true.
+Proof.
+  intros.
+  destruct p1; destruct p2; try discriminate.
+  simpl in *. destruct (peq b b0); try discriminate.
+  subst.
+  apply peq_true.
+Qed.
+
+Lemma sameblock_trans : forall p1 p2 p3,
+  sameblock p1 p2 = true ->
+  sameblock p2 p3 = true->
+  sameblock p1 p3 = true.
+Proof.
+  intros.
+  destruct p1; try discriminate.
+  destruct p2; try discriminate.
+  destruct p3; try discriminate.
+  simpl in *.
+  destruct (peq b b0); try discriminate.
+  destruct (peq b0 b1); try discriminate.
+  subst.
+  apply peq_true.
+Qed.
+
 Lemma body_sumarray: semax_body Vprog Gprog f_sumarray sumarray_spec.
 Proof.
   start_function. forward. forward. forward.
   (*Is this the right way to refer to curr?*)
   assert (0 < size) by admit.
   forward_while
-  (EX (i: Z) b ,
-   PROP (0 <= i <= size; a = (Vptr b (Ptrofs.repr 0)))
-   LOCAL (temp _end (Vptr b (Ptrofs.repr (size * 4)));
-      temp _curr (Vptr b (Ptrofs.repr (i * 4)));
+  (EX (i: Z),
+   PROP (0 <= i <= size)
+   LOCAL (temp _end (offset_val (size * 4) a);
+      temp _curr (offset_val (i * 4) a);
       temp _a a; temp _n (Vint (Int.repr size));
       temp _s (Vint (Int.repr (sum_Z (sublist 0 i contents)))))
    SEP (data_at sh (tarray tuint size) (map Vint (map Int.repr contents)) a)).
-  - admit.
-    (* Exists 0%Z. entailer!. *)
-  - 
-    (* [0, n) 0 .. n-1
-    valid: Vptr b 0 .. Vptr b (n-1)
-    weak_valid: Vptr b 0 .. Vptr b (n-1), Vptr b n *)
-    entailer!. unfold test_order_ptrs. simpl.
-    destruct (proj_sumbool (peq b b)) eqn:Hpeq; unfold proj_sumbool in Hpeq; rewrite peq_true in Hpeq;
-      try discriminate; clear Hpeq.
+  - Exists 0%Z. entailer!. simpl sizeof. list_solve.
+  - entailer!. rewrite isptr_denote_tc_test_order; auto.
+    unfold test_order_ptrs. simpl.
+    assert (Hsameblock : sameblock (offset_val (i * 4) a)
+      (offset_val (Zlength (map Vint (map Int.repr contents)) * 4) a) = true). {
+      apply sameblock_trans with a.
+      apply sameblock_symm. apply isptr_offset_val_sameblock. apply H5.
+      apply isptr_offset_val_sameblock. apply H5.
+    }
+    simpl_reptype.
+    rewrite Hsameblock.
     sep_apply data_at_memory_block.
     apply andp_right.
     + sep_eapply memory_block_weak_valid_pointer.
       * instantiate (1 := (i * 4)).
-        simpl. Zlength_solve.
-      * simpl. Zlength_solve.
+        simpl. list_solve.
+      * simpl. list_solve.
       * apply readable_nonidentity; auto.
-      * apply derives_refl'. f_equal.
-        simpl. f_equal.
-        admit.
+      * auto.
     + sep_eapply memory_block_weak_valid_pointer.
-    * instantiate (1 := (Zlength contents) * 4).
-      simpl. list_solve.
-    * simpl. Zlength_solve.
-    * apply readable_nonidentity; auto.
-    * apply derives_refl'. f_equal.
-      simpl. f_equal.
-      admit.
-  (*entailer!. simpl. Search denote_tc_test_order. rewrite isptr_denote_tc_test_order. *) 
+      * instantiate (1 := (Zlength contents) * 4).
+        simpl. list_solve.
+      * simpl. list_solve.
+      * apply readable_nonidentity; auto.
+      * list_solve.
 Admitted.
