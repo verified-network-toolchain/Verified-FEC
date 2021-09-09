9c338faee2e221a8589c5a784e003d34a6ff25c2
Andrew W. Appel <appel@princeton.edu>
2021-09-09 13:26:23 -0400

    Update license-header of fec.h

--- src/fecActuator/fec.h
+++ src/fecActuator/fec.h
@@ -4,13 +4,31 @@
  *
  * Copyright 2021 Peraton Labs Inc.
  *
+ * Originally developed by A. McAuley, Peraton Labs Inc.
+ * 
+ * This software was developed in work supported by the following U.S.
+ * Government contracts:
+ *
+ * W15P7T-05-C-R402
+ * HR0011-15-C-0098
+ * HR0011-20-C-0160
+ *
+ * Any opinions, findings and conclusions or recommendations expressed in
+ * this material are those of the author(s) and do not necessarily reflect
+ * the views, either expressed or implied, of the U.S. Government.
+
+ * This software embodies U.S. Patent 5,115,436
+ *
+ * DoD Distribution Statement A
+ * Approved for Public Release, Distribution Unlimited
+ * 
+ * DISTAR Case 34797, cleared June 21, 2021.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * See the beginning of fec.c for other disclosures and release notes.
  */
 
 #ifndef _FEC_H
