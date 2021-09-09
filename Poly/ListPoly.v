793689781fc869a2f241e1da4e183201ba2902dd
josh <joshcohen98@gmail.com>
2021-09-09 11:07:47 -0400

    Add license info to .v files

--- Poly/ListPoly.v
+++ Poly/ListPoly.v
@@ -1,3 +1,11 @@
+(* Copyright 2021 Joshua M. Cohen
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*)
+
 (*We define polynomials as lists of field elements so that we can compute with them, unlike mathcomp's. However,
   to use the theorems in mathcomp, we relate lpolys to polys*)
 From mathcomp Require Import all_ssreflect.
