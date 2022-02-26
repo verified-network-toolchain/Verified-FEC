77009cf834bb012a4ece9b6bb8a490d78a10c6b5
josh <joshcohen98@gmail.com>
2022-02-26 18:24:37 -0500

    Function to find parity index for decoder

--- proofs/FEC/RSEEncoderDecoder.v
+++ proofs/FEC/RSEEncoderDecoder.v
@@ -5,6 +5,10 @@ Require Import AbstractEncoderDecoder.
 Require Import ReedSolomonList.
 Require Import ZSeq.
 From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Set Bullet Behavior "Strict Subproofs".
 
 (*TODO: move*)
 (*Construct Inhabitant instance automatically*)
@@ -46,7 +50,7 @@ Canonical fec_packet_act_eqType := EqType fec_packet_act fec_packet_act_eqMixin.
 #[export]Instance packet_act_inhab : Inhabitant fec_packet_act := 
   mk_fecpkt packet_inhab fec_data_inhab.
 
-(* Representing Blocks *)
+(** Representing Blocks *)
 
 Record block := mk_blk { blk_id: int;
   data_packets: list (option fec_packet_act); parity_packets: list (option fec_packet_act); blk_k : Z; blk_h : Z}.
@@ -83,7 +87,7 @@ Definition stats (b: block) : list byte :=
   map (fun x => match x with
                 | None => Byte.one
                 | Some _ => Byte.zero
-                end) ((data_packets b) ++ (parity_packets b)).
+                end) (data_packets b).
 
 Definition lengths (b: block) : list Z :=
   map (@Zlength byte) (packet_mx b) ++ 
@@ -95,7 +99,6 @@ Definition num_receieved (b: block) : Z :=
 (*Max of list of nonnegative integers*)
 Definition list_max_nonneg {A: Type} (f: A -> Z) (l: list A) : Z :=
   fold_right (fun x y => Z.max (f x) y) (-1) l.
-
 
 (*Get current value of c for the block. If block is not complete, this will be -1.
   It is OK to take max of only packet_contents because parities have Zlength (contents) = c
@@ -158,6 +161,8 @@ Definition get_blocks (l: list fec_packet_act) : list block :=
   2. All packets with same blockIndex have same h
   Then we can reason about permutation, not subseq*)
 
+(** Encoder predicate *)
+
 (*Finally, we can define the encoder predicate*)
 Definition enc : (encoder fec_data) :=
   fun (orig : list packet) (encoded: list fec_packet_act) =>
@@ -176,10 +181,146 @@ Definition enc : (encoder fec_data) :=
                                                           | Some p => f_packet p :: acc
                                                           end) nil (data_packets b) ) blocks).
 
-(*TODO: handle (i:Z) by finding value*)
+(** Decoder predicate *)
+
+(*A block is "recoverable" if we have at least k total packets*)
+Definition recoverable (b: block) : bool :=
+  Z_ge_lt_dec (Zlength (filter isSome (data_packets b)) + Zlength (filter isSome (parity_packets b))) 
+    (Zlength (data_packets b)) .
+
+(*[decoder_list] takes in a value i, representing the sublist of parities that we will look at
+  to find xh parity packets. We will write a function to find that value so the user doesn't need
+  to know it. TODO: maybe move to ReedSolomonList.v*)
+
+Definition find_decoder_list_param (b: block) : Z :=
+  let numMissing := Zlength (filter isNone (data_packets b)) in
+  match (pick (fun (i: ordinal (Z.to_nat (Zlength (parity_packets b) + 1))) => 
+    Z.eq_dec (Zlength (filter isSome (sublist 0 (Z.of_nat i) (parity_packets b)))) numMissing)) with
+  | None => -1
+  | Some i => Z.of_nat i
+  end.
+
+(*Lengths of stats, parity_mx*)
+Lemma parity_mx_sublist_length: forall (i: nat) (s: seq (option fec_packet_act)) (*(b: block)*),
+Zlength [seq x <- sublist 0 (Z.of_nat i) (* (Z.of_nat (nat_of_ord i))*) s (*(parity_packets b)*) | isSome x] =
+Zlength
+  [seq x <- sublist 0 (Z.of_nat i) (* (Z.of_nat (nat_of_ord i))*)
+              [seq match x with
+                   | Some p => Some (p_contents (f_packet p))
+                   | None => None
+                   end
+                 | x <- s (*parity_packets b *)]
+     | isSome x].
+Proof.
+  move => i. elim : i => [s //= | i IH s].
+  have->: (Z.of_nat i.+1) = Z.of_nat i + 1 by lia.
+  have[Hin | Hout]: (Z.of_nat i + 1 <= Zlength s \/ Z.of_nat i + 1 >= Zlength s) by lia.
+  - rewrite !(sublist_split 0 (Z.of_nat i) (Z.of_nat i + 1)); try lia; last first.
+      rewrite Zlength_map; lia.
+    rewrite !sublist_len_1; try(rewrite Zlength_map); try lia.
+    rewrite !filter_cat !Zlength_app IH /=. f_equal.
+    rewrite !Znth_map; try lia.
+    by case : (Znth (Z.of_nat i) s).
+  - rewrite !sublist_same_gen; try (rewrite Zlength_map); try lia.
+    (*Case for whole list - easiest to do another induction here*)
+    rewrite {IH Hout i}. elim : s => [//= | h t /= IH].
+    case : h => [//= a | //=]. by rewrite !Zlength_cons IH.
+Qed. 
+
+Lemma proj_sumbool_false: forall {P Q: Prop} (a: {P} + {Q}), Coqlib.proj_sumbool a = false -> Q.
+Proof.
+  move => P Q a.
+  by case: a.
+Qed. 
+
+Lemma nat_pred_bound: forall (m n: nat),
+  (m < n)%N ->
+  (m.-1 < n)%N.
+Proof.
+  move => m n Hmn.
+  apply (leq_ltn_trans (leq_pred m) Hmn).
+Qed.
+
+Definition nat_pred_ord {n : nat} (x: 'I_n) : 'I_n :=
+  Ordinal (nat_pred_bound (ltn_ord x)).
+  
+(*The condition we need for [decoder_list_correct]*)
+Lemma find_decoder_list_param_correct: forall (b: block),
+  recoverable b ->
+  Zlength (filter (fun x => Z.eq_dec (Byte.signed x) 1) (stats b)) =
+  Zlength (filter isSome (sublist 0 (find_decoder_list_param b) (parity_mx b))).
+Proof.
+  move => b.
+  rewrite /recoverable => Hge. apply proj_sumbool_true in Hge.
+  rewrite /find_decoder_list_param.
+  case: (pick (fun (i: ordinal (Z.to_nat (Zlength (parity_packets b) + 1))) => 
+    Z.eq_dec (Zlength (filter isSome (sublist 0 (Z.of_nat i) (parity_packets b)))) 
+    (Zlength (filter isNone (data_packets b))))) /pickP =>[i Hpick |Hpick].
+  - apply proj_sumbool_true in Hpick.
+    rewrite /stats filter_map /= !Zlength_map /preim. 
+    rewrite (@eq_in_filter _ _ isNone); last first.
+    + move => x. case : x => [x' /= Hinx| /=Hinx]. 
+      * by rewrite /isNone Byte.signed_zero.
+      * by rewrite /isNone Byte.signed_one.
+    + rewrite -Hpick /parity_mx.
+      (*prove this case in separate lemma*)
+      apply parity_mx_sublist_length.
+  - (*Now we show that such a value has to exist*)
+    (*Let's show that for all i, the length is smaller, not just non-equal
+      TODO: generalize this? Doesn't depend on blocks/parities, etc. Is it useful?*)
+    have Hlt: (forall (i:'I_(Z.to_nat (Zlength (parity_packets b) + 1) )), 
+      (Zlength [seq x <- sublist 0 (Z.of_nat i) (parity_packets b) | isSome x]) <
+           (Zlength [seq x <- data_packets b | isNone x])). {
+      move => i. remember (nat_of_ord i) as x. move: i Heqx. elim : x => [/= i Hi|n IH i Hi].
+      - (*if length 0, then we use 0 for Hpick - get contradiction*) 
+        rewrite Zlength_nil. have [//|Heq]: (0 < Zlength [seq x <- data_packets b | isNone x] \/
+          Zlength [seq x <- data_packets b | isNone x] = 0) by list_solve.
+        move: Hpick => /(_ i). rewrite Heq =>Hpick. apply proj_sumbool_false in Hpick.
+        move: Hpick. by rewrite -Hi /= Zlength_nil.
+      - (*ok: use n=i.-1 (as ord), then split into sublist 0 n and last elt, 
+          get from IH that lt, since +1, has to be < or <=, but cant be equal bf of Hpick, contradiction*)
+        have Hi1n: n = (nat_pred_ord i) by rewrite /= -Hi -pred_Sn.
+        move: IH => /(_ (nat_pred_ord i) Hi1n) => Hlens.
+        have->:(Z.of_nat n.+1) = Z.of_nat n + 1 by lia.
+        have Hbound: Z.of_nat n + 1 <= Zlength (parity_packets b). {
+          have /ltP: (n.+1 < Z.to_nat (Zlength (parity_packets b) + 1))%N by rewrite Hi. lia. }
+        rewrite (sublist_split 0 (Z.of_nat n) (Z.of_nat n + 1)); try lia.
+        rewrite filter_cat Zlength_app sublist_len_1 /=; try lia.
+        case Hnth: (Znth (Z.of_nat n) (parity_packets b)) => [y //= |//=].
+        + rewrite Zlength_cons Zlength_nil /=. 
+          have[//|Heq]: (Zlength [seq x <- sublist 0 (Z.of_nat n) (parity_packets b) | isSome x] + 1 < 
+            Zlength [seq x <- data_packets b | isNone x] \/
+             Zlength [seq x <- sublist 0 (Z.of_nat n) (parity_packets b) | isSome x] + 1 = 
+            Zlength [seq x <- data_packets b | isNone x]) by lia.
+          move: Hpick => /(_ i). (*TODO: better way than repeating this again*)
+          rewrite -Hi. have->:(Z.of_nat n.+1) = Z.of_nat n + 1 by lia.
+          rewrite (sublist_split 0 (Z.of_nat n) (Z.of_nat n + 1)); try lia.
+          rewrite filter_cat Zlength_app sublist_len_1 /=; try lia.
+          rewrite Hnth /= Zlength_cons Zlength_nil /= => Hneq.
+          by apply proj_sumbool_false in Hneq.
+      + rewrite Zlength_nil. lia. }
+    (*Now we know that |found packets| + |found parities | >= k
+      and |found packets| = k - |missing packets|, so
+        |found parities| >= |missing packets|, a contradiction of Hlt
+      when we instantiate with (Zlength parities)*)
+    have Hpacklen: Zlength [seq x <- data_packets b | isSome x] + Zlength [seq x <- data_packets b | isNone x] =
+      Zlength (data_packets b). {
+      (*I will be lazy and prove by induction*)
+      rewrite {Hge Hpick Hlt}. elim : (data_packets b) => [// | h t /= IH].
+        case : h => [a /= | /=]; rewrite !Zlength_cons; lia. }
+    have Hgeq: Zlength [seq x <- (parity_packets b) | isSome x] >= 
+      Zlength [seq x <- data_packets b | isNone x]. {
+      move: Hge. rewrite -Hpacklen => Hge.
+      apply Z.ge_le in Hge. apply Zplus_le_reg_l in Hge. by apply Z.le_ge. (*why doesnt lia work?*) }
+      have Hi: (Z.to_nat (Zlength (parity_packets b)) < (Z.to_nat (Zlength (parity_packets b) + 1)))%N. {
+        apply /ltP. rewrite Z2Nat.inj_add; try lia. list_solve. }
+      move: Hlt => /(_ (Ordinal Hi)) /=. rewrite Z2Nat.id; [| list_solve].
+      rewrite sublist_same; lia.
+Qed.
+
+(*Now we use [find_decoder_list_param] to find the appropriate sublist of parities*)
 Definition decoder_list_block (b: block) : list (list byte) :=
-  (*blk_h is just a placeholder*)
-  decoder_list (blk_k b) (blk_c b) (packet_mx b) (parity_mx b) (stats b) (lengths b) (blk_h b).
+  decoder_list (blk_k b) (blk_c b) (packet_mx b) (parity_mx b) (stats b) (lengths b) (find_decoder_list_param b).
 
 (*TODO: need to transform packet bytes back to packet (with IP fields, etc) - this will be annoying
   also may not need IP/UDP stuff here: see*)
@@ -205,3 +346,5 @@ Definition dec : (decoder fec_data) :=
       f_packet fp = p) \/
     (exists (b: block) (i: Z), 0 <= i < (blk_k b) /\ In b blocks /\ 
       Some p = packet_from_bytes (Znth i (decoder_list_block b))). 
+
+(** Encoder/Decoder correctness*)
