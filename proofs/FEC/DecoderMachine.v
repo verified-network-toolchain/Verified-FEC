941d51dc99e0890b15f5888750b300506639fb06
josh <joshcohen98@gmail.com>
2022-11-11 17:36:23 -0500

    Sequence number comparison
    Split into separate file
    Finish proofs
    Generalize to any wordsize
    Use correct seqnum comparison in machine functional model

--- proofs/FEC/DecoderMachine.v
+++ proofs/FEC/DecoderMachine.v
@@ -9,6 +9,7 @@ Require Import ZSeq.
 Require Import Block.
 Require Import DecoderTimeouts.
 Require Import DecoderGeneric.
+Require Import SeqCmp.
 From mathcomp Require Import all_ssreflect.
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -16,6 +17,19 @@ Unset Printing Implicit Defensive.
 Set Bullet Behavior "Strict Subproofs".
 From RecordUpdate Require Import RecordSet. (*for updating records easily*)
 Import RecordSetNotations.
+
+Module S64:=SeqCmp Wordsize_64.
+Module S32:=SeqCmp Wordsize_32.
+
+(*Coq module weirdness: it does not recognize
+  S32.I.int and Int.int as equal, though they are.
+  We can give a trivial conversion function*)
+Definition sint (i: int) : S32.I.int :=
+  S32.I.mkint (Int.intval i) (Int.intrange i).
+
+Definition sint64 (i: int64) : S64.I.int :=
+  S64.I.mkint (Int64.intval i) (Int64.intrange i).
+
 Section Machine.
 
 Definition create_block_with_fec_packet_m (p: fpacket) (time : int) : 
@@ -94,251 +108,21 @@ Fixpoint upd_time_m (time: int) (curr: fpacket) (blocks: list block) :
       if (block_notin_packet bl curr) 
       then Int.add time Int.one 
       else time
-    else if Int64.ltu currSeq blkid then Int.add time Int.one
+    else if S64.seq_lt (sint64 currSeq) (sint64 blkid) 
+      then Int.add time Int.one
     else upd_time_m time curr tl
   end.
 
-  (*Here, we use *)
-
-
-(*Integer comparison is more difficult - we don't want
-  just Int.ltu - since 0 should be considered larger than
-  2^32-1. The following function is based on "Serial number
-  arithmetic" in RFC 1982 and the wikipedia article*)
-  (*We use this for comparing times*)
-(**
-Definition seq_comp (i1 i2: int) : Z :=
-  if Int.eq i1 i2 then 0%Z else
-  if Int.ltu b a then Int.signed (Int.sub i1 i2)
-  else 
-
-
-
-  Definition compare (a b: Z) : Z :=
-  if Z.eq_dec a b then 0%Z else 
-  if Z_gt_le_dec a b then Z.modulo (a - b) Int.modulus (*TODO: see*) else
-  Int.max_unsigned - (b - a - 1).
-*)
-(*From netinet/tcp_seq.h*)
-(*This function determines if a sequence number is less than another.
-If |i1 -i2| = 2^31, then this returns true even though the value is
-undefined.*)
-Definition int_seq_lt (i1 i2: int) : bool :=
-  Z_lt_ge_dec (Int.signed (Int.sub i1 i2)) 0.
-
-(*Proof that this works*)
-(*RFC spec for when i1 is considered smaller than i2
-  (i1 and i2 are s1 and s2 in the spec, a1 and a2 are i1 and i2 in
-  the spec)*)
-Definition serial_lt (i1 i2: int) : Prop :=
-  let a1 := Int.unsigned i1 in
-  let a2 := Int.unsigned i2 in
-  (a1 < a2 /\ a2 - a1 < Int.half_modulus) \/
-  (a1 > a2 /\ a1 - a2 > Int.half_modulus).
-
-  (*From verified-packet-reorder*)
-  (*OK, both are equivalent - so I can do either*)
-  (*TODO: copy proof/modify as needed, prove properties
-    THEN, we will prove something like: because of the reordering
-    bound, every time we compare two packet sequence numbers,
-    we can determine unambiguously if the difference is
-    bigger or smaller than 2^31 and therefore prove that the comparison
-    of nats is equivalent to one on ints
-    (we will also need a similar but easier for result for equality)
-
-    To do this, we need some kind of invariant - likely just that
-    blockId of each block is seqNum of some packet, then use
-    [u_seqNum_reorder_bound] - hmm do we need to relate to time at all?
-    dont think so
-    ah but we do - because this wouldnt work if we kept everything
-    need to know that at most threshold unique packets between
-    (weaker than with timeouts where we could unique packets exactly)
-    so u_seqNum could only have increased by threshold at most
-    (could be much less if we consider duplicates as newly seen)
-    so this means that difference between u_seqNums can be at most
-    threshold, so 
-    hmmm think about this more
-    
-    it must
-    be the case that one of the [serial_lt] cases applies and hence
-    the [int_seq_lt] function is equivalent to *)
-  Lemma test i1 i2:
-    Int.signed (Int.sub i1 i2) =
-    Int.signed (Int.repr ((Int.unsigned i1 - Int.unsigned i2) mod Int.modulus)).
-  Proof.
-    rewrite /Int.sub !Int.signed_repr_eq.
-    by rewrite !Zmod_mod.
-  Qed.
-
-  (*2 options:
-  Int.signed (Int.sub i1 i2) OR
-  Int.signed (Int.repr ((Int.unsigned i1 - Int.unsigned i2) mod Int.modulus))*)
-  
-Lemma Int_signed_repr_eq_sign: forall x,
-  0 <= x <= Int.max_unsigned ->
-  Int.signed (Int.repr x) > 0 <-> 0 < x < Int.half_modulus.
-Proof.
-  move=> x Hx. rewrite Int.signed_repr_eq. 
-  have->: x mod Int.modulus = x by rewrite Zmod_small; rep_lia.
-  case: zlt; rep_lia.
-Qed.
-
-Lemma Int_signed_repr_eq_sign_neg: forall x,
-  0 <= x <= Int.max_unsigned ->
-  Int.signed (Int.repr x) < 0 <-> x >= Int.half_modulus.
-Proof.
-  move=> x Hx.  rewrite Int.signed_repr_eq. 
-  have ->: x mod Int.modulus = x by rewrite Zmod_small; rep_lia.
-  case: (zlt x Int.half_modulus); rep_lia.
-Qed.
-
-Definition z_abs_diff (z1 z2: Z) : Z :=
-  Z.abs (z1 - z2).
-
-Lemma z_abs_diff_max: forall z1 z2,
-  z_abs_diff z1 z2 =
-  Z.max (z1 - z2) (z2 - z1).
-Proof.
-  move=> z1 z2. rewrite /z_abs_diff. lia.
-Qed.
-
-Definition int_diff (i1 i2: int) :=
-  z_abs_diff (Int.unsigned i1) (Int.unsigned i2).
-
-Lemma int_diff_le (i1 i2 : int):
-  Int.unsigned i1 <= Int.unsigned i2 ->
-  int_diff i1 i2 = Int.unsigned i2 - (Int.unsigned i1).
-Proof.
-  move=> Hle. rewrite /int_diff /Int.sub z_abs_diff_max. 
-  rewrite Z.max_r; try lia.
-Qed.
-
-Lemma int_diff_ge (i1 i2: int):
-  Int.unsigned i1 >= Int.unsigned i2 ->
-  int_diff i1 i2 = Int.unsigned i1 - (Int.unsigned i2).
-Proof.
-  move=> Hge. rewrite /int_diff /Int.sub z_abs_diff_max.
-  rewrite Z.max_l; try lia.
-Qed.
-
-Lemma int_seq_lt_correct (i1 i2: int):
-  reflect 
-  (int_diff i1 i2 = Int.half_modulus \/ serial_lt i1 i2)
-  (int_seq_lt i1 i2) .
-Proof.
-  rewrite /serial_lt/int_seq_lt/=.
-  case:  (Z_lt_ge_dec (Int.signed (Int.sub i1 i2)) 0) => [Hlt | Hge]/=.
-  - apply ReflectT. move: Hlt.
-    rewrite /Int.sub.
-    case : (Z_lt_ge_dec (Int.unsigned i2) (Int.unsigned i1) ) => Hi12/=.
-    + rewrite Int_signed_repr_eq_sign_neg; try rep_lia.
-      rewrite int_diff_ge; lia.
-    + rewrite Int.signed_repr_eq.
-      case: (Z.eq_dec (Int.unsigned i1) (Int.unsigned i2)) => Heq.
-      * rewrite Heq Z.sub_diag /= Zmod_0_l. lia.
-      * rewrite -(Z_mod_plus_full _ 1) !Zmod_small; try rep_lia.
-        case: zlt => Hhalf; try rep_lia. move => _.
-        rewrite int_diff_le; rep_lia.
-  - apply ReflectF => C. move: Hge.
-    rewrite /Int.sub.
-    case : (Z_lt_ge_dec (Int.unsigned i2) (Int.unsigned i1) ) => Hi12/=.
-    + rewrite Int.signed_repr_eq Zmod_small; try rep_lia.
-      case: zlt => [Hlt | Hge]; try rep_lia.
-      (*Contradiction from C*)
-      rewrite int_diff_ge in C; rep_lia.
-    + rewrite int_diff_le in C; try rep_lia. 
-      case: (Z.eq_dec (Int.unsigned i1) (Int.unsigned i2)) => Heq; 
-      first by rep_lia.
-      rewrite Int.signed_repr_eq  -(Z_mod_plus_full _ 1) !Zmod_small; 
-      try rep_lia.
-      case: zlt => Hhalf; rep_lia.
-Qed.
-
-(*Is this lemma true?*)
-(*This cannot be true as written - z2 could be much much larger*)
-Lemma div_le (n m: Z):
-  0 < m ->
-  0 <= n ->
-  n / m <= n.
-Proof.
-  move=> Hm Hn.
-  have:=(Zdiv_interval_2 0 n n m). lia.
-Qed.
-
-Lemma z_abs_diff_sym: forall z1 z2,
-  z_abs_diff z1 z2 = z_abs_diff z2 z1.
-Proof.
-  move=> z1 z2. rewrite /z_abs_diff. lia.
-Qed.
-
-Lemma abs_diff_mod (z1 z2 n: Z):
-  0 < n ->
-  z_abs_diff z1 z2 < n ->
-  z_abs_diff (z1 mod (2 * n)) (z2 mod (2 * n)) <> n.
-Proof.
-  move=> Hn.
-  wlog: z1 z2 /z1 <= z2; last first.
-  - move=> Hle.
-    rewrite !z_abs_diff_max (Z.max_r _ (z2 - z1)); try lia.
-    set (m := 2 * n).
-    move => Hlt C.
-    case: (Z.max_spec_le (z1 mod m - z2 mod m) 
-      (z2 mod m - z1 mod m)) => 
-      [[Hmodle Hmax] | [Hmodle Hmax]].
-    + rewrite Hmax in C.
-      have: z2 mod m - z1 mod m = (z2 - z1) mod m. {
-        rewrite Zminus_mod (Zmod_small (z2 mod m - z1 mod m)); try lia.
-      }
-      rewrite C Zmod_small; try lia. 
-    + rewrite Hmax in C. (*TODO: repetitive*)
-      have: z1 mod m - z2 mod m = (z1 - z2) mod m. {
-        rewrite Zminus_mod (Zmod_small (z1 mod m - z2 mod m)); try lia. 
-      }
-      rewrite C.
-      have Hlower: z1 - z2 > - n by lia.
-      case: (Z.eq_dec z1 z2) => Heq.
-      * (*minor case: when equal, show n = 0*)
-        rewrite Heq Z.sub_diag /= Zmod_0_l. lia.
-      * rewrite -(Z_mod_plus_full _ 1) Zmod_small; lia.
-  - move=> Hwlog Habs.
-    have [H1 | H2]: (z1 <= z2 \/ z2 <= z1) by lia.
-    + by apply Hwlog.
-    + rewrite z_abs_diff_sym. apply Hwlog=>//.
-      by rewrite z_abs_diff_sym.
-Qed.
-
-(*TODO: start here*)
-
-(*Now we will want to prove: if the unsigned value between
-  any two Z is < Int.half_modulus, then this equals the integer lt function*)
-Lemma int_seq_lt_lt (z1 z2: Z):
-  z_abs_diff z1 z2 < Int.half_modulus ->
-  int_seq_lt (Int.repr z1) (Int.repr z2) = Z.ltb z1 z2.
-Proof.
-  rewrite /z_abs_diff => Habs.
-  case: (int_seq_lt (Int.repr z1) (Int.repr z2)) /int_seq_lt_correct.
-  - move => [Heq | Hlt].
-    + move: Heq. rewrite /int_diff !Int.unsigned_repr_eq.
-      move=> Hdiff.
-      (*Hmm, how do we do this?*)
-      exfalso. apply (@abs_diff_mod z1 z2 Int.half_modulus); try rep_lia.
-      by rewrite /z_abs_diff.
-      have->//: 2 * Int.half_modulus = Int.modulus by [].
-    + (*Now do lt case*)
-
-(*We need an integer comparison function that works for any
-  2 ints *)
-
-Definition int_leu (i1 i2: int) : bool :=
-  Int.ltu i1 i2 || Int.eq i1 i2.
+(*TODO: may need to add to SeqCmp.v - SEE*)
+Definition seq_leq (i1 i2: int) : bool :=
+  Int.eq i1 i2 || S32.seq_lt (sint i1) (sint i2).
 
 (*Timeouts if threshold exceeded*)
 (*Hmm, what happens if time wraps around? - maybe for this 
 we should do sequence number comparison because times will 
 always be within threshold?*)
 Definition not_timed_out_m: int -> block -> bool := fun currTime =>
-  (fun b => int_leu currTime (Int.add (Int.repr(black_time b)) threshold)).
-
+  (fun b => seq_leq currTime (Int.add (Int.repr(black_time b)) threshold)).
 
 Fixpoint update_dec_state_m (blocks: list block) (curr: fpacket)
   (time: int) : list block * list packet:=
@@ -346,12 +130,12 @@ match blocks with
 | nil => let t := create_block_with_packet_black_m curr time in 
     ([:: t.1], t.2)
 | bl :: tl =>
-  let currSeq := Int.repr (Z.of_nat (fd_blockId curr)) in
-  let blkid := Int.repr (Z.of_nat (blk_id bl)) in
-  if Int.eq currSeq blkid then
+  let currSeq := Int64.repr (Z.of_nat (fd_blockId curr)) in
+  let blkid := Int64.repr (Z.of_nat (blk_id bl)) in
+  if Int64.eq currSeq blkid then
     let t := add_packet_to_block_black_m curr bl in
     (t.1 :: tl, t.2)
-    else if Int.ltu currSeq blkid then
+    else if S64.seq_lt (sint64 currSeq) (sint64 blkid) then
     let t := create_block_with_packet_black_m curr time in 
       (t.1 :: blocks, t.2)
   else 
---
+++ proofs/FEC/SeqCmp.v
@@ -0,0 +1,250 @@
+(*Comparing Sequence Numbers for an arbitrary wordsize*)
+Require Import VST.floyd.functional_base.
+From Coq Require Import ssreflect.
+
+Module SeqCmp(WS: WORDSIZE).
+
+(*Integer comparison is more difficult - we don't want
+  just Int.ltu - since 0 should be considered larger than
+  2^32-1. The following function is based on "Serial number
+  arithmetic" in RFC 1982 and the wikipedia article*)
+  (*We use this for comparing times*)
+
+Module I := Make WS.
+
+Ltac rep_lia_setup2 ::=
+pose_lemmas I.unsigned I.unsigned_range;
+pose_lemmas I.signed I.signed_range;
+unfold I.max_unsigned in *;
+pose proof I.half_modulus_pos;
+pose proof I.half_modulus_modulus.
+
+Ltac rep_nia :=
+  rep_lia_setup; rep_lia_setup2; nia.
+
+
+(*From netinet/tcp_seq.h*)
+(*This function determines if a sequence number is less than another.
+If |i1 -i2| = I.half_modulus, then this returns true even 
+though the comparison value should be undefined.*)
+Definition seq_lt (i1 i2: I.int) : bool :=
+  Z_lt_ge_dec (I.signed (I.sub i1 i2)) 0.
+
+
+(*Proof that this works*)
+(*RFC spec for when i1 is considered smaller than i2
+  (i1 and i2 are s1 and s2 in the spec, a1 and a2 are i1 and i2 in
+  the spec)*)
+Definition serial_lt (i1 i2: I.int) : Prop :=
+  let a1 := I.unsigned i1 in
+  let a2 := I.unsigned i2 in
+  (a1 < a2 /\ a2 - a1 < I.half_modulus) \/
+  (a1 > a2 /\ a1 - a2 > I.half_modulus).
+
+(*Part 1: Prove that [seq_lt] is true exactly when
+  [serial_lt] holds or the two values are I.half_modulus apart*)
+
+Definition z_abs_diff (z1 z2: Z) : Z :=
+  Z.abs (z1 - z2).
+
+Lemma z_abs_diff_max: forall z1 z2,
+  z_abs_diff z1 z2 =
+  Z.max (z1 - z2) (z2 - z1).
+Proof.
+  move=> z1 z2. rewrite /z_abs_diff. lia.
+Qed.
+
+Definition int_diff (i1 i2: I.int) :=
+  z_abs_diff (I.unsigned i1) (I.unsigned i2).
+
+Lemma int_diff_le (i1 i2 : I.int):
+  I.unsigned i1 <= I.unsigned i2 ->
+  int_diff i1 i2 = I.unsigned i2 - (I.unsigned i1).
+Proof.
+  move=> Hle. rewrite /int_diff z_abs_diff_max. 
+  rewrite Z.max_r; try lia.
+Qed.
+
+Lemma int_diff_ge (i1 i2: I.int):
+  I.unsigned i1 >= I.unsigned i2 ->
+  int_diff i1 i2 = I.unsigned i1 - (I.unsigned i2).
+Proof.
+  move=> Hge. rewrite /int_diff z_abs_diff_max.
+  rewrite Z.max_l; try lia.
+Qed.
+
+Lemma I_signed_repr_eq_sign_neg: forall x,
+  0 <= x <= I.max_unsigned ->
+  I.signed (I.repr x) < 0 <-> x >= I.half_modulus.
+Proof.
+  move=> x Hx.  rewrite I.signed_repr_eq. 
+  have ->: x mod I.modulus = x by rewrite Zmod_small; rep_lia.
+  case: (zlt x I.half_modulus); rep_lia.
+Qed.
+
+Lemma seq_lt_correct (i1 i2: I.int):
+  reflect 
+  (int_diff i1 i2 = I.half_modulus \/ serial_lt i1 i2)
+  (seq_lt i1 i2) .
+Proof.
+  rewrite /serial_lt/seq_lt/=.
+  case:  (Z_lt_ge_dec (I.signed (I.sub i1 i2)) 0) => [Hlt | Hge]/=.
+  - apply ReflectT. move: Hlt.
+    rewrite /I.sub.
+    case : (Z_lt_ge_dec (I.unsigned i2) (I.unsigned i1) ) => Hi12/=.
+    + rewrite I_signed_repr_eq_sign_neg; try rep_lia.
+      rewrite int_diff_ge; lia.
+    + rewrite I.signed_repr_eq.
+      case: (Z.eq_dec (I.unsigned i1) (I.unsigned i2)) => Heq.
+      * rewrite Heq Z.sub_diag /= Zmod_0_l. case:zlt; rep_lia. 
+      * rewrite -(Z_mod_plus_full _ 1) !Zmod_small; try rep_lia.
+        case: zlt => Hhalf; try rep_lia. move => _.
+        rewrite int_diff_le; rep_lia.
+  - apply ReflectF => C. move: Hge.
+    rewrite /Int.sub.
+    case : (Z_lt_ge_dec (I.unsigned i2) (I.unsigned i1) ) => Hi12/=.
+    + rewrite I.signed_repr_eq Zmod_small; try rep_lia.
+      case: zlt => [Hlt | Hge]; try rep_lia.
+      (*Contradiction from C*)
+      rewrite int_diff_ge in C; rep_lia.
+    + rewrite int_diff_le in C; try rep_lia. 
+      case: (Z.eq_dec (I.unsigned i1) (I.unsigned i2)) => Heq; 
+      first by rep_lia.
+      rewrite I.signed_repr_eq  -(Z_mod_plus_full _ 1) !Zmod_small; 
+      try rep_lia.
+      case: zlt => Hhalf; rep_lia.
+Qed.
+
+(*Part 2: If we have two Z values which are less than I.half_modulus
+  apart, then the sequence comparison function is equivalent to the
+  Z lt function. This is the wraparound property that we want.*)
+
+(*First, some intermediate lemmas*)
+
+Lemma div_le (n m: Z):
+  0 < m ->
+  0 <= n ->
+  n / m <= n.
+Proof.
+  move=> Hm Hn.
+  have:=(Zdiv_interval_2 0 n n m). lia.
+Qed.
+
+Lemma z_abs_diff_sym: forall z1 z2,
+  z_abs_diff z1 z2 = z_abs_diff z2 z1.
+Proof.
+  move=> z1 z2. rewrite /z_abs_diff. lia.
+Qed.
+
+(*The big intermediate lemma: two ints separated by n
+  cannot have their difference mode (2 * n) be n*)
+Lemma abs_diff_mod (z1 z2 n: Z):
+  0 < n ->
+  z_abs_diff z1 z2 < n ->
+  z_abs_diff (z1 mod (2 * n)) (z2 mod (2 * n)) <> n.
+Proof.
+  move=> Hn.
+  wlog: z1 z2 /z1 <= z2; last first.
+  - move=> Hle.
+    rewrite !z_abs_diff_max (Z.max_r _ (z2 - z1)); try lia.
+    set (m := 2 * n).
+    move => Hlt C.
+    case: (Z.max_spec_le (z1 mod m - z2 mod m) 
+      (z2 mod m - z1 mod m)) => 
+      [[Hmodle Hmax] | [Hmodle Hmax]].
+    + rewrite Hmax in C.
+      have: z2 mod m - z1 mod m = (z2 - z1) mod m. {
+        rewrite Zminus_mod (Zmod_small (z2 mod m - z1 mod m)); try lia.
+      }
+      rewrite C Zmod_small; try lia. 
+    + rewrite Hmax in C. (*TODO: repetitive*)
+      have: z1 mod m - z2 mod m = (z1 - z2) mod m. {
+        rewrite Zminus_mod (Zmod_small (z1 mod m - z2 mod m)); try lia. 
+      }
+      rewrite C.
+      have Hlower: z1 - z2 > - n by lia.
+      case: (Z.eq_dec z1 z2) => Heq.
+      * (*minor case: when equal, show n = 0*)
+        rewrite Heq Z.sub_diag /= Zmod_0_l. lia.
+      * rewrite -(Z_mod_plus_full _ 1) Zmod_small; lia.
+  - move=> Hwlog Habs.
+    have [H1 | H2]: (z1 <= z2 \/ z2 <= z1) by lia.
+    + by apply Hwlog.
+    + rewrite z_abs_diff_sym. apply Hwlog=>//.
+      by rewrite z_abs_diff_sym.
+Qed.
+
+Lemma Zplus_gt: forall a b c d,
+  a > b ->
+  c > d ->
+  a + c > b + d.
+Proof.
+  lia.
+Qed.
+
+(*The theorem we want:*)
+(*Lots of tedious cases, and we need to do extra work
+  because rep_lia doesn't know the constants*)
+Lemma seq_lt_lt (z1 z2: Z):
+  z_abs_diff z1 z2 < I.half_modulus ->
+  seq_lt (I.repr z1) (I.repr z2) = Z.ltb z1 z2.
+Proof.
+  rewrite /z_abs_diff => Habs.
+  case: (seq_lt (I.repr z1) (I.repr z2)) /seq_lt_correct.
+  - move => [Heq | Hlt].
+    + move: Heq. rewrite /int_diff !I.unsigned_repr_eq.
+      move=> Hdiff.
+      exfalso. apply (@abs_diff_mod z1 z2 I.half_modulus); try rep_lia.
+      by rewrite /z_abs_diff.
+      have->//: 2 * I.half_modulus = I.modulus by rep_lia.
+    + (*Now do lt case*)
+      move: Hlt. rewrite /serial_lt !I.unsigned_repr_eq.
+      case: (z1 <? z2) /Z.ltb_spec => // Hleq.
+      rewrite Z.abs_eq in Habs; try lia.
+      have Hz1eq:=(Z_div_mod_eq_full z1 I.modulus).
+      have Hz2eq:=(Z_div_mod_eq_full z2 I.modulus).
+      have Hdiv:=Hleq.
+      apply (Z.div_le_mono _ _ I.modulus) in Hdiv;try rep_lia.
+      apply Z.le_lteq in Hdiv. rep_nia.
+  - move => Hnot. case: (z1 <? z2) /Z.ltb_spec =>// Hlt.
+    move: Habs.
+    have->: Z.abs (z1 - z2) = z2 - z1 by lia. move=> Habs.
+    exfalso. apply: Hnot.
+    right. (*left case cannot happen by [abs_diff_mod]*)
+    rewrite /serial_lt !I.unsigned_repr_eq.
+    have Hz1eq:=(Z_div_mod_eq_full z1 I.modulus).
+    have Hz2eq:=(Z_div_mod_eq_full z2 I.modulus).
+    have Hdiv: z1 <= z2 by lia.
+    apply (Z.div_le_mono _ _ I.modulus) in Hdiv; try rep_lia.
+    have Hsub:(
+      z1 mod I.modulus <= z2 mod I.modulus ->
+      z2 mod I.modulus - z1 mod I.modulus =
+        (z2 - z1) mod I.modulus). {
+      move=> H.
+      rewrite Zminus_mod (Zmod_small (_ - _)); try rep_lia. 
+      split;
+      have:=(Z.mod_pos_bound z1 I.modulus);
+      have:=(Z.mod_pos_bound z2 I.modulus); rep_lia.
+    }
+    case: (Z.lt_total (z1 mod I.modulus) (z2 mod I.modulus)) => 
+    [Hgt21 | [Heq21 | Hgt12]]; try rep_nia.
+    + have Hmod0: (z2 - z1) mod I.modulus = 0 by lia.
+      have:=(Z_div_mod_eq_full (z2 - z1) I.modulus).
+      rewrite Hmod0 Z.add_0_r.
+      have: (0 <= ((z2 - z1) / I.modulus)) by
+        apply Z.div_pos; rep_lia.
+      rewrite Z.le_lteq => [[Hgt0 | Heq0]]; try rep_lia.
+      rep_nia.
+      (*Again, need to know that multiply with something >= 1, 
+      get that thing*)
+    + right. split=>//; try rep_lia.
+      have->:z1 mod I.modulus - z2 mod I.modulus =
+        (z1 - z2) + I.modulus * ((z2 / I.modulus) - (z1 / I.modulus)) by lia.
+      have: z1 - z2 + I.modulus * (z2 / I.modulus - z1 / I.modulus) >
+      - I.half_modulus + I.modulus; try rep_lia.
+      move: Hdiv.
+      rewrite Z.le_lteq => [[Hgt0 | Heq0]]; rep_nia.
+Qed.
+
+End SeqCmp.
+
